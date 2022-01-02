1ee708c38180c4f298299391d82bd1576de56efd
vignesh <vigne@Vicky-PC>
2022-01-02 16:53:58 +0530

    This is to export the project skeleton in to git

---
+++ src/test/java/automationTest/EclipseSetup.java
@@ -0,0 +1,11 @@
+package automationTest;
+
+public class EclipseSetup {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Project");
+		System.out.println("This is to export the project to eclipse");
+
+	}
+
+}
