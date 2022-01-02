5df2ad18145d6fbc367a56fc5b243216a5ef9eac
vignesh <vigne@Vicky-PC>
2022-01-02 17:34:26 +0530

    correcting the first class

--- src/test/java/automationTest/EclipseSetup.java
+++ src/test/java/automationTest/EclipseSetup.java
@@ -4,9 +4,7 @@ public class EclipseSetup {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Project");
-		System.out.println("This is to export the project to eclipse");
-		System.out.println("another");
-
+		
 	}
 
 }
